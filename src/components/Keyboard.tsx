361909d892fad13392c24804d532a152fcc1ecb9
David Chica <david.chikhb@gmail.com>
2023-06-08 13:06:03 -0500

    Files uploaded

---
+++ src/components/Keyboard.tsx
@@ -0,0 +1,72 @@
+import "./Keyboard.css";
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+type KeyboardProps = {
+  activeLetter: string[];
+  disabled?: boolean;
+  inactiveLetters: string[];
+  addGuessedLetter: (letter: string) => void;
+};
+
+const Keyboard = ({
+  activeLetter,
+  disabled = false,
+  inactiveLetters,
+  addGuessedLetter,
+}: KeyboardProps) => {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gap: ".5rem",
+      }}
+    >
+      {KEYS.map((key) => {
+        const isActive = activeLetter.includes(key);
+        const isInactive = inactiveLetters.includes(key);
+        return (
+          <button
+            onClick={() => addGuessedLetter(key)}
+            key={key}
+            className={`btn ${isActive ? "active" : ""} ${
+              isInactive ? "inactive" : ""
+            }`}
+            disabled={isActive || isInactive || disabled}
+          >
+            {key}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Keyboard;
